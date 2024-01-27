647cd32c31556c74896de5cdbe5c9a6d498512c0
nanditha2020 <141707361+nanditha2020@users.noreply.github.com>
2024-01-27 11:13:20 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import questions from "./questions";
+import Result from "./components/Result";
+import QuestionBox from "./components/QuestionBox";
+
+
+
+function App() {
+
+  return (
+    <div>
+      <QuestionBox/>
+    </div>
+  );
+}
+
+export default App;
